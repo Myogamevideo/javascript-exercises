af58010a40a928908ad251c0747ddb1651774e90
emilie lamy <lamyemilie82@gmail.com>
2018-11-06 10:31:17 +0100

    exercice 2.2

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -9,10 +9,42 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
 
     var performOperation = function(operation) {
+
+        var a = parseInt(document.getElementById("op-one").value);
+        var b = parseInt(document.getElementById("op-two").value);
+
+        switch (operation){
+            case "addition":
+            var result = a+b;
+            alert(result);
+
+            break;
+
+            case "substraction":
+            var result = a-b;
+            alert(result);
+
+            break;
+
+            case "multiplication":
+            var result = a*b;
+            alert(result);
+
+            break;
+
+            case "division":
+            var result = a/b;
+            alert(result);
+
+            break;
+        }
+
         // perform the operation
     };
 
@@ -22,3 +54,53 @@
         });
     });
 })();
+
+
+
+/* PREVIOUS EXERCISE
+
+
+(function() {
+    // to get the value of an input: document.getElementById("element-id").value
+
+    document.getElementById("addition").addEventListener("click", function() {
+        var a = parseInt(document.getElementById("op-one").value);
+        var b = parseInt(document.getElementById("op-two").value);
+
+        var result = a+b;
+        alert(result);
+
+        // perform an addition
+    });
+
+    document.getElementById("substraction").addEventListener("click", function() {
+        var a = parseInt(document.getElementById("op-one").value);
+        var b = parseInt(document.getElementById("op-two").value);
+
+        var result = a-b;
+        alert(result);        
+        
+        // perform an substraction
+    });
+
+    document.getElementById("multiplication").addEventListener("click", function() {
+        var a = parseInt(document.getElementById("op-one").value);
+        var b = parseInt(document.getElementById("op-two").value);
+
+        var result = a*b;
+        alert(result);
+        
+        // perform an multiplication
+    });
+
+    document.getElementById("division").addEventListener("click", function() {
+        var a = parseInt(document.getElementById("op-one").value);
+        var b = parseInt(document.getElementById("op-two").value);
+
+        var result = a/b;
+        alert(result);
+        
+        // perform an division
+    });
+})();
+*/
