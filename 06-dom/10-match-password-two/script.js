cbe88f8322e9d51f16854d5ef2d84c665dbc7f8c
emilie lamy <lamyemilie82@gmail.com>
2018-11-14 16:56:26 +0100

    6.10 en cours

--- 06-dom/10-match-password-two/script.js
+++ 06-dom/10-match-password-two/script.js
@@ -9,8 +9,33 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
 
     // your code here
+    //set attribute avec class et error 
+
+
+
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+    
+        var inputOne = document.getElementById("pass-one").value;
+
+        var inputTwo = document.getElementById("pass-two").value;
+
+
+        if(inputOne != inputTwo){
+            document.getElementById("pass-one").style.border = "1px solid red";
+            document.getElementById("pass-two").style.border = "1px solid red"; 
+            
+        } else {
+            document.getElementById("pass-two").style.border = "0px"; 
+            document.getElementById("pass-one").style.border = "0px"; 
+        }
+
+        console.log(inputOne,inputTwo);
+
+        });
 
 })();
